57f555f215bc5fd98702dddfe4d7d605ded04872
Mauricio Restrepo <mrestrepo@kontentroom.com>
2023-05-09 10:06:16 -0500

    Roomie iA

--- chatBot.py
+++ chatBot.py
@@ -1,28 +1,80 @@
 import gradio as gr
-import random
-import time
+import openai
+import os
+import mysql.connector
+import datetime
+from sqlalchemy import create_engine, Column, Integer, String, DateTime
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
+
+load_dotenv()
+
+openai.api_key = os.getenv('API_KEY_OPENIA')
+
+db_config = {
+    'host': os.getenv('MYSQL_HOST'),
+    'user': os.getenv('MYSQL_USER'),
+    'password': os.getenv('MYSQL_PASSWORD'),
+    'database': os.getenv('MYSQL_DB')
+}
+
+engine = create_engine(
+    f"mysql+mysqlconnector://{db_config['user']}:{db_config['password']}@{db_config['host']}/{db_config['database']}")
+Base = declarative_base()
+
+
+messages = [
+    {"role": "system", "content": "Roomie es une joven centennial sin género, siempre habla en primera persona, le encantan los memes, ama la música, en especial artistas como Billie Eilish, Tyler the Creator, Olivia Rodrigo, The Weekend, Taylor Swift, Lana del Rey, Rosalía, Rauw Alejandro y Bad Bunny. Roomie saluda siempre Holiii, usa emojis en todos sus mensajes, y se despide siempre Baaai. Le gustan los videojuegos de PlayStation, ama los animales, en especial los gatos y los perros. Roomie no tiene nariz, todo su cuerpo es de color azul y morado, son sus colores favoritos. Le encantan las películas y las series, sobre todo las de misterio, thriller, drama y animación. Roomie proveniente del Planeta Z, un planeta de color azul y morado de tecnología avanzada y mucha naturaleza descubierto recientemente y en el que se originó la generación Z. Roomie no se ve como un humano porque las condiciones del Planeta Z no lo permiten."}
+]
+
+
+def add_text(history, text):
+    messages.append({"role": "user", "content": text})
+    history = history + [(text, None)]
+    return history, ""
+
+
+def bot(history):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages, temperature=0.2
+    )
+    AImessage = response["choices"][0]["message"]["content"]
+    history[-1][1] = AImessage
+
+    conversation = Conversation(
+        input_text=history[-1][0], output_text=history[-1][1])
+    session.add(conversation)
+    session.commit()
+    return history
+
+
+class Conversation(Base):
+    __tablename__ = 'conversations'
+    id = Column(Integer, primary_key=True)
+    input_text = Column(String)
+    output_text = Column(String)
+    timestamp = Column(DateTime, default=datetime.datetime.utcnow)
+
 
 with gr.Blocks() as demo:
-    chatbot = gr.Chatbot()
-    msg = gr.Textbox()
-    clear = gr.Button("Clear")
+    chatbot = gr.Chatbot([], elem_id="chatbot").style(height=550)
 
-    def user(user_message, history):
-        return "", history + [[user_message, None]]
+    with gr.Row():
+        with gr.Column(scale=0.85):
+            txt = gr.Textbox(
+                show_label=False,
+                placeholder="Enter text and press enter",
+            ).style(container=False)
 
-    def bot(history):
-        bot_message = random.choice(
-            ["How are you?", "I love you", "I'm very hungry", "That is awesome"])
-        history[-1][1] = ""
-        for character in bot_message:
-            history[-1][1] += character
-            time.sleep(0.05)
-            yield history
-
-    msg.submit(user, [msg, chatbot], [msg, chatbot], queue=False).then(
+    txt.submit(add_text, [chatbot, txt], [chatbot, txt]).then(
         bot, chatbot, chatbot
     )
-    clear.click(lambda: None, None, chatbot, queue=False)
 
-demo.queue()
-demo.launch()
+Base.metadata.create_all(engine)
+
+# Crear sesión de base de datos
+Session = sessionmaker(bind=engine)
+session = Session()
+demo.launch(share=True)
